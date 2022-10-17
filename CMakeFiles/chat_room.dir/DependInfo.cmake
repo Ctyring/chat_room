13f446a2a5177cb6f5a8d2ee581519b93ec903c9
ctyring <173479693@qq.com>
2022-10-17 11:07:28 +0800

    完成项目创建

---
+++ CMakeFiles/chat_room.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/cty/chat_room/chat/my_module.cc" "/root/cty/chat_room/CMakeFiles/chat_room.dir/chat/my_module.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "chat_room_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/apps/sylar/include"
+  "sylar"
+  "sylar/thirdpart"
+  "sylar/thirdpart/yaml-cpp/include"
+  "sylar/thirdpart/jsoncpp-1.8.4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/chat_room.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_room.dir/chat/my_module.cc.o"
+  "lib/libchat_room.pdb"
+  "lib/libchat_room.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_room.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
